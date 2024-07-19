4381c2d8e762ed36c1a0f862641413ff728f64cb
ligelowbee <36973865+ligelowbee@users.noreply.github.com>
2024-07-19 10:20:19 -0230

    Add files via upload

--- calibre-cover-wallpaper.sh
+++ calibre-cover-wallpaper.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 #Make labeled wallpaper from calibre covers
-artd="/home/yargo/Pictures/Wallpaper/"
-origc=$(find ~/Ebooks/calibre -name "cover.jpg" | shuf -n1)
+calibred="$HOME/Ebooks/calibre"
+artd="$HOME/Pictures/Wallpaper/"
+origc=$(find "$calibred" -name "cover.jpg" | shuf -n1)
 fname=$(awk 'BEGIN {FS="/"};{printf "%s - %s.jpg",$(NF-2),$(NF-1)}' <<< "$origc")
 cd "$artd"
-cp "$origc" "$fname"
+if [ ! -e "$fname" ]; then
+    cp "$origc" "$fname"
+fi
+
 label-wallpaper.sh "$fname"
 if [ $? -eq 0 ]; then
     mv -f "labeled-$fname" "$fname"
